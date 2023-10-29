4afe5daebaf155a72bc1e81ec241be6fcfc44b80
Vladimir Katin <katin.v.v@gmail.com>
2023-10-29 18:26:47 +0000

    Update dag

--- airflow/dags/youtube_comments_score.py
+++ airflow/dags/youtube_comments_score.py
@@ -1,7 +1,8 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-import pendulum
 import datetime as dt
+
+from settings import SCRIPTS_PATH
  
 args = {
     "owner": "dzailz",
@@ -12,24 +13,24 @@ args = {
 }
  
 with DAG(
-    dag_id='youtube_comments_score',
+    dag_id="youtube_comments_score",
     default_args=args,
     schedule_interval=None,
-    tags=['youtube', 'score'],
+    tags=["youtube", "score"],
 ) as dag:
-    get_data = BashOperator(task_id='get_data',
-                            bash_command="python3 /home/dzailz/project/scripts/get_data.py",
+    get_data = BashOperator(task_id="get_data",
+                            bash_command=f"python3 {SCRIPTS_PATH}get_data.py",
                             dag=dag)
-    process_data = BashOperator(task_id='process_data',
-                            bash_command="python3 /home/dzailz/project/scripts/process_data.py",
+    process_data = BashOperator(task_id="process_data",
+                            bash_command=f"python3 {SCRIPTS_PATH}process_data.py",
                             dag=dag)
-    train_test_split_data = BashOperator(task_id='train_test_split_data',
-                            bash_command="python3 /home/dzailz/project/scripts/train_test_split_data.py",
+    train_test_split_data = BashOperator(task_id="train_test_split_data",
+                            bash_command=f"python3 {SCRIPTS_PATH}train_test_split_data.py",
                             dag=dag)  
-    train_model = BashOperator(task_id='train_model',
-                            bash_command="python3 /home/dzailz/project/scripts/train_model.py",
+    train_model = BashOperator(task_id="train_model",
+                            bash_command=f"python3 {SCRIPTS_PATH}train_model.py",
                             dag=dag)
-    test_model = BashOperator(task_id='test_model',
-                            bash_command="python3 /home/dzailz/project/scripts/test_model.py",
+    test_model = BashOperator(task_id="test_model",
+                            bash_command=f"python3 {SCRIPTS_PATH}test_model.py",
                             dag=dag)
     get_data >> process_data >> train_test_split_data >> train_model >> test_model
