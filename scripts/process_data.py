6c24249bedfe728c1aa604c295467899d07d76e8
alduinsh <lucy.mihaylichenko@gmail.com>
2023-11-06 16:56:07 +0300

    add changes scripts

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,11 +1,22 @@
 import pandas as pd
+import mlflow
+import os
 
-from settings import DATASETS_PATH
- 
+from settings import DATASETS_PATH, BASIC_PATH, ML_FLOW_TRACKING_URI, SCRIPTS_PATH
+os.environ["MLFLOW_REGISTRY_URI"] = f"{BASIC_PATH}mlflow/"
+mlflow.set_tracking_uri(ML_FLOW_TRACKING_URI)
+mlflow.set_experiment("process_data")
+
 df = pd.read_csv(f'{DATASETS_PATH}data.csv', header=None)
- 
-df[0] = (df[0]-df[0].min())/(df[0].max()-df[0].min())
- 
-with open(f'{DATASETS_PATH}data_processed.csv', 'w') as f:
-    for i, item in enumerate(df[0].values):
-        f.write("{},{}\n".format(i, item))
+
+with mlflow.start_run():
+   df[0] = (df[0] - df[0].min()) / (df[0].max() - df[0].min())
+
+   with open(f'{DATASETS_PATH}data_processed.csv', 'w') as f:
+      for i, item in enumerate(df[0].values):
+            f.write("{},{}\n".format(i, item))
+
+   mlflow.log_artifact(local_path=f"{SCRIPTS_PATH}process_data.py",
+                           artifact_path="process_data code")
+   mlflow.end_run()
+
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,29 +1,36 @@
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 import mlflow
-
-import pandas as pd
- 
-
 from settings import DATASETS_PATH, SCRIPTS_PATH, MODELS_PATH, ML_FLOW_TRACKING_URI
- 
+
 mlflow.set_tracking_uri(ML_FLOW_TRACKING_URI)
 mlflow.set_experiment("train_model")
- 
-df = pd.read_csv(f'{DATASETS_PATH}data_train.csv', header=None)
-df.columns = ['id', 'counts']
-model = LinearRegression()
- 
-with mlflow.start_run():
-    mlflow.sklearn.log_model(model,
-                             artifact_path="lr",
-                             registered_model_name="lr")
-    mlflow.log_artifact(local_path=f"{SCRIPTS_PATH}train_model.py",
-                        artifact_path="train_model code")
-    mlflow.end_run()
- 
-model.fit(df['id'].values.reshape(-1,1), df['counts'])
- 
-with open(f'{MODELS_PATH}data.pickle', 'wb') as f:
-    pickle.dump(model, f)
+
+def load_data():
+    df = pd.read_csv(f"{DATASETS_PATH}/data_train.csv", header=None)
+    df.columns = ['id', 'counts']
+    return df
+
+def train_model(df):
+    model = LinearRegression()
+    model.fit(df['id'].values.reshape(-1,1), df['counts'])
+    return model
+
+def save_model(model):
+    with open(f'{MODELS_PATH}data.pickle', 'wb') as f:
+        pickle.dump(model, f)
+
+def log_metrics(model):
+    with mlflow.start_run():
+        mlflow.sklearn.log_model(model, artifact_path="lr", registered_model_name="lr")
+        mlflow.log_artifact(local_path=f"{SCRIPTS_PATH}/train_model.py", artifact_path="train_model code")
+        mlflow.end_run()
+
+if __name__ == "__main__":
+    df = load_data()
+    model = train_model(df)
+    save_model(model)
+    log_metrics(model)
+
  
