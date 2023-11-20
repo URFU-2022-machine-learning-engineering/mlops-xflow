7a7a94469e7f331b3e9339f676b0e1851af3c547
OrlovAlexandr <35524327+OrlovAlexandr@users.noreply.github.com>
2023-11-20 17:29:24 +0500

    Comments added

--- scripts/train_test_split_data.py
+++ scripts/train_test_split_data.py
@@ -3,14 +3,17 @@ import numpy as np
 
 from settings import DATASETS_PATH 
 
+# Загрузка обработанных данных
 df = pd.read_csv(f'{DATASETS_PATH}data_processed.csv', header=None)
- 
+
+# Разделение данных на обучающую и тестовую выборки
 idxs = np.array(df.index.values)
 np.random.shuffle(idxs)
 l = int(len(df)*0.7)
 train_idxs = idxs[:l]
 test_idxs = idxs[l+1:]
- 
+
+# Запись данных
 df.loc[train_idxs, :].to_csv(f'{DATASETS_PATH}data_train.csv',
                         header=None,
                         index=None)
