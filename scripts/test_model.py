32a861ff857776741432e58d09a8ae564dde88fa
OrlovAlexandr <35524327+OrlovAlexandr@users.noreply.github.com>
2023-11-20 17:21:24 +0500

    Update test_model.py. Added comments

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -3,13 +3,17 @@ import pickle
 import pandas as pd
 
 from settings import MODELS_PATH, DATASETS_PATH
- 
+
+# Загрузка данных для тестирования модели
 df = pd.read_csv(f'{DATASETS_PATH}data_test.csv', header=None)
 df.columns = ['id', 'counts']
  
 model = LinearRegression()
+
+# Загрузка ранее сохраненной модели
 with open(f'{MODELS_PATH}data.pickle', 'rb') as f:
     model = pickle.load(f)
- 
+
+# Оценка модели на тестовых данных и вывод результата
 score = model.score(df['id'].values.reshape(-1,1), df['counts'])
 print("score=", score)
