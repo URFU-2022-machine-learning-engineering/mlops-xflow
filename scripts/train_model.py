98fe75e4bf479c465b11dc8564663cfe5954e17a
OrlovAlexandr <35524327+OrlovAlexandr@users.noreply.github.com>
2023-11-20 17:23:59 +0500

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -6,14 +6,17 @@ import pandas as pd
  
 
 from settings import DATASETS_PATH, SCRIPTS_PATH, MODELS_PATH, ML_FLOW_TRACKING_URI
- 
+
+# Установка URI для отслеживания экспериментов в MLflow
 mlflow.set_tracking_uri(ML_FLOW_TRACKING_URI)
 mlflow.set_experiment("train_model")
- 
+
+# Загрузка данных для обучения модели
 df = pd.read_csv(f'{DATASETS_PATH}data_train.csv', header=None)
 df.columns = ['id', 'counts']
 model = LinearRegression()
- 
+
+# Запуск нового эксперимента в MLflow
 with mlflow.start_run():
     mlflow.sklearn.log_model(model,
                              artifact_path="lr",
@@ -21,9 +24,9 @@ with mlflow.start_run():
     mlflow.log_artifact(local_path=f"{SCRIPTS_PATH}train_model.py",
                         artifact_path="train_model code")
     mlflow.end_run()
- 
+
+# Обучение модели на данных и сохранение её в файл 'data.pickle'
 model.fit(df['id'].values.reshape(-1,1), df['counts'])
- 
 with open(f'{MODELS_PATH}data.pickle', 'wb') as f:
     pickle.dump(model, f)
  
