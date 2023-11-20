8e1940fe0b1b832edba10934db3b05a3f7407057
OrlovAlexandr <35524327+OrlovAlexandr@users.noreply.github.com>
2023-11-20 17:31:46 +0500

    Comments added

--- airflow/dags/youtube_comments_score.py
+++ airflow/dags/youtube_comments_score.py
@@ -11,13 +11,16 @@ args = {
     "retry_delays": dt.timedelta(minutes=1),
     "depends_on_past": False
 }
- 
+
+# Определение DAG с идентификатором "youtube_comments_score"
+# и установка параметров и расписания выполнения
 with DAG(
     dag_id="youtube_comments_score",
     default_args=args,
     schedule_interval=None,
     tags=["youtube", "score"],
 ) as dag:
+    # Определение задач
     get_data = BashOperator(task_id="get_data",
                             bash_command=f"python3 {SCRIPTS_PATH}get_data.py",
                             dag=dag)
@@ -33,4 +36,6 @@ with DAG(
     test_model = BashOperator(task_id="test_model",
                             bash_command=f"python3 {SCRIPTS_PATH}test_model.py",
                             dag=dag)
+
+    # Определение порядка выполнения задач
     get_data >> process_data >> train_test_split_data >> train_model >> test_model
